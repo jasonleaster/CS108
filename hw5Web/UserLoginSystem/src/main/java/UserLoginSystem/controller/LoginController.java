f8d8e48863aafc459b9c2edd15c2feb68d11caae
jasonleaster <jasonleaster@gmail.com>
2016-10-26 20:06:19 +0800

    hw5

---
+++ hw5Web/UserLoginSystem/src/main/java/UserLoginSystem/controller/LoginController.java
@@ -0,0 +1,15 @@
+package UserLoginSystem.controller;
+
+/**
+ * File name  : hw5.LoginController.java
+ * Programmer : EOF (Jason Leaster)
+ * Date       : 2016/10/24
+ * Email      : jasonleaster@gmail.com
+ * Descirption:
+ */
+
+import org.springframework.stereotype.Controller;
+
+
+public class LoginController {
+}
---
+++ hw5Web/UserLoginSystem/src/main/java/UserLoginSystem/dao/test.java
@@ -0,0 +1,11 @@
+package UserLoginSystem.dao;
+
+/**
+ * File name  : UserLoginSystem.controller.dao
+ * Programmer : EOF (Jason Leaster)
+ * Date       : 2016/10/24
+ * Email      : jasonleaster@gmail.com
+ * Descirption:
+ */
+public class test {
+}
