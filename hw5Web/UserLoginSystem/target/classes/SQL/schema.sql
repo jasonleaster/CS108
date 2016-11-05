e76b38c9fe8eb9e8fb554bf889c0bb69a84f762e
jasonleaster <jasonleaster@gmail.com>
2016-11-05 17:25:34 +0800

    update

--- hw5Web/UserLoginSystem/target/classes/SQL/schema.sql
+++
@@ -1,9 +0,0 @@
-SET FOREIGN_KEY_CHECKS=0;
-
-drop table if exists users;
-
-CREATE TABLE users (
-    username VARCHAR(25) NOT NULL,
-    email VARCHAR(50) PRIMARY KEY NOT NULL,
-    password VARCHAR(25) NOT NULL
-);
